36d82138f7eff31705a5080d7d0ceea9a6032312
S-Azhar <121962868+S-Azhar@users.noreply.github.com>
2024-03-26 17:35:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,129 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+  { songName: "Tu hai kahan", filePath: "songs/1.mp3", coverPath: "covers/1.jpeg" },
+  { songName: "Pehle Bhi Main", filePath: "songs/2.mp3", coverPath: "covers/2.jpeg" },
+  { songName: "Heartless (feat Aastha Gill)", filePath: "songs/3.mp3", coverPath: "covers/3.jpeg" },
+  { songName: "Blue Eyes", filePath: "songs/4.mp3", coverPath: "covers/4.jpeg" },
+  { songName: "Heeriye (feat Arijit Singh)", filePath: "songs/5.mp3", coverPath: "covers/5.jpeg" },
+    { songName: "Kabhi Kabhi Aditi", filePath: "songs/6.mp3", coverPath: "covers/6.jpeg" },
+  { songName: "Street Celebrity", filePath: "songs/7.mp3", coverPath: "covers/7.jpeg" },
+  { songName: "Arabic Kuthu - Halamithi Habibo", filePath: "songs/8.mp3", coverPath: "covers/8.jpeg" },
+  { songName: "Ik Tera", filePath: "songs/9.mp3", coverPath: "covers/9.jpeg" },
+  { songName: "Gone Girl", filePath: "songs/6.mp3", coverPath: "covers/10.jpeg" },
+
+
+
+
+
+
+
+  { songName: "Street Celebrity", filePath: "songs/7.mp3", coverPath: "covers/7.jpeg" },
+  { songName: "Pehle Bhi Main", filePath: "songs/2.mp3", coverPath: "covers/2.jpeg" },
+  { songName: "Sher Khul Gaye", filePath: "songs/9.mp3", coverPath: "covers/11.jpeg" },
+  { songName: "Tu hai kahan", filePath: "songs/1.mp3", coverPath: "covers/1.jpeg" },
+  { songName: "Heeriye (feat Arijit Singh)", filePath: "songs/8.mp3", coverPath: "covers/5.jpeg" },
+  { songName: "Arabic Kuthu - Halamithi Habibo", filePath: "songs/8.mp3", coverPath: "covers/8.jpeg" },
+  { songName: "Badass (From Leo)", filePath: "songs/4.mp3", coverPath: "covers/12.jpeg" },
+  { songName: "Blue Eyes", filePath: "songs/5.mp3", coverPath: "covers/4.jpeg" },
+  { songName: "Gone Girl", filePath: "songs/6.mp3", coverPath: "covers/10.jpeg" },
+  { songName: "Heartless (feat Aastha Gill)", filePath: "songs/3.mp3", coverPath: "covers/3.jpeg" },
+  { songName: "Heeriye (feat Arijit Singh)", filePath: "songs/8.mp3", coverPath: "covers/8.jpeg" },
+  { songName: "Ik Tera", filePath: "songs/9.mp3", coverPath: "covers/9.jpeg" },
+  { songName: "Kabhi Kabhi Aditi", filePath: "songs/10.mp3", coverPath: "covers/10.jpeg" },
+]
+
+songItems.forEach((element, i) => {
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
+
+
+// Handle play/pause click
+masterPlay.addEventListener('click', () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+    gif.style.opacity = 1;
+  }
+  else {
+    audioElement.pause();
+    masterPlay.classList.remove('fa-pause-circle');
+    masterPlay.classList.add('fa-play-circle');
+    gif.style.opacity = 0;
+  }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', () => {
+  // Update Seekbar
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', () => {
+  audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+})
+
+const makeAllPlays = () => {
+  Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.classList.remove('fa-pause-circle');
+    element.classList.add('fa-play-circle');
+  })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+  element.addEventListener('click', (e) => {
+    makeAllPlays();
+    songIndex = parseInt(e.target.id);
+    e.target.classList.remove('fa-play-circle');
+    e.target.classList.add('fa-pause-circle');
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    gif.style.opacity = 1;
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+  })
+})
+
+document.getElementById('next').addEventListener('click', () => {
+  if (songIndex >= 9) {
+    songIndex = 0
+  }
+  else {
+    songIndex += 1;
+  }
+  audioElement.src = `songs/${songIndex+1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove('fa-play-circle');
+  masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', () => {
+  if (songIndex <= 0) {
+    songIndex = 0
+  }
+  else {
+    songIndex -= 1;
+  }
+  audioElement.src = `songs/${songIndex+1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove('fa-play-circle');
+  masterPlay.classList.add('fa-pause-circle');
+})
